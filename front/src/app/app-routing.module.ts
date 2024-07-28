c5a389679dc29794a8b860f6e86ad3ca98de7568
Laura <laura@upschool.fr>
2024-07-28 10:51:35 +0200

    components initialised, routing done

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './product/products/products.component';
+import { ProductsAdminComponent } from './product/products-admin/products-admin.component';
 
 const routes: Routes = [
-
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductsComponent},
+  {path: 'admin/products', component: ProductsAdminComponent}
 ];
 
 @NgModule({
